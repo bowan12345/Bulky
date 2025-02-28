7e3ac0615a5b510c242afbc61c2eb0e3e8ef19b4
bowan12345 <bowan.ding@gmail.com>
2025-02-28 13:34:38 +1300

    read category list

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Electronics" , DisplayOrder = 1},
+                new Category { Id = 2, Name = "Clothing", DisplayOrder = 3 },
+                new Category { Id = 3, Name = "Home Appliances", DisplayOrder = 2}
+                );
+        }
+    }
+}
